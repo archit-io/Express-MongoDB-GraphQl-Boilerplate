2fe597a3c2dfb19cf9ae0b5668bcc8871bdcaca4
Archit Choudhary <archit.digisocial@gmail.com>
2022-10-20 09:57:57 +0530

    Added sample graphql mutations

--- schema/schema.js
+++ schema/schema.js
@@ -2,6 +2,8 @@ const graphql = require('graphql');
 const Book = require('../models/book');
 const Author = require('../models/Author');
 
+
+
 const {
    GraphQLObjectType, GraphQLString,
    GraphQLID, GraphQLInt,GraphQLSchema,
@@ -9,14 +11,16 @@ const {
 } = graphql;
 
 
-//Schema defines data on the Graph like object types(book type), the relation between
-//these object types and describes how they can reach into the graph to interact with
+
+//Schema defines data on the Graph like object types(book type), relation between
+//these object types and describes how it can reach into the graph to interact with
 //the data to retrieve or mutate the data  
+
 
 
 const BookType = new GraphQLObjectType({
    name: 'Book',
-   //We are wrapping fields in the function as we don’t want to execute this until
+   //We are wrapping fields in the function as we dont want to execute this ultil
    //everything is inilized. For example below code will throw an error AuthorType not
    //found if not wrapped in a function
    fields: () => ({
@@ -51,7 +55,7 @@ const AuthorType = new GraphQLObjectType({
 
 
 
-//RootQuery describes how users can use the graph and grab data.
+//RootQuery describe how users can use the graph and grab data.
 //E.g Root query to get all authors, get all books, get a particular
 //book or get a particular author.
 const RootQuery = new GraphQLObjectType({
@@ -62,7 +66,7 @@ const RootQuery = new GraphQLObjectType({
            //argument passed by the user while making the query
            args: { id: { type: GraphQLID } },
            resolve(parent, args) {
-               //Here we define how to get data from a database source
+               //Here we define how to get data from database source
 
 
 
@@ -93,8 +97,50 @@ const RootQuery = new GraphQLObjectType({
    }
 });
 
+
+//Very similar to RootQuery helps users to add/update to the database.
+const Mutation = new GraphQLObjectType({
+   name: 'Mutation',
+   fields: {
+       addAuthor: {
+           type: AuthorType,
+           args: {
+               //GraphQLNonNull make these fields required
+               name: { type: new GraphQLNonNull(GraphQLString) },
+               age: { type: new GraphQLNonNull(GraphQLInt) }
+           },
+           resolve(parent, args) {
+               let author = new Author({
+                   name: args.name,
+                   age: args.age
+               });
+               return author.save();
+           }
+       },
+       addBook:{
+           type:BookType,
+           args:{
+               name: { type: new GraphQLNonNull(GraphQLString)},
+               pages: { type: new GraphQLNonNull(GraphQLInt)},
+               authorID: { type: new GraphQLNonNull(GraphQLID)}
+           },
+           resolve(parent,args){
+               let book = new Book({
+                   name:args.name,
+                   pages:args.pages,
+                   authorID:args.authorID
+               })
+               return book.save()
+           }
+       }
+   }
+});
+
+
+
 //Creating a new GraphQL Schema, with options query which defines query
-//we will allow users to use it when they are making requestsuntil.
+//we will allow users to use when they are making requests.
 module.exports = new GraphQLSchema({
-   query: RootQuery
+   query: RootQuery,
+   Mutation: Mutation
 });
