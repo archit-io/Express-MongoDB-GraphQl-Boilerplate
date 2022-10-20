8bb7e5883b6a976418b4a47376fbc5d7fc05be94
Archit Choudhary <archit.digisocial@gmail.com>
2022-10-20 09:29:31 +0530

    Corrected the graphqlHTTP import statement

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
  
  
-const graphqlHTTP = require('express-graphql');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
  
 const schema = require('./schema/schema')
  
